f5e09f27d385c47062e12b23457f3196052152bc
jtx-thinh <58506385+jtx-thinh@users.noreply.github.com>
2021-07-29 13:27:11 +0700

    Delete MinGW_WorkDir directory

--- MinGW_WorkDir/TestCampaign/build/3707052758/DISH_WHITEBOX/vcast_preprocess.8660.11.c
+++
@@ -1,9 +0,0 @@
-
-typedef int VECTORCAST_MARKER__UNIT_PREFIX_START;
-
-typedef int VECTORCAST_MARKER__UNIT_PREFIX_END;
-#include "C:/VCAST/Training/SourceFiles/dishwasher_fsm.c"
-
-typedef int VECTORCAST_MARKER__UNIT_APPENDIX_START;
-
-typedef int VECTORCAST_MARKER__UNIT_APPENDIX_END;
--- MinGW_WorkDir/TestCampaign/build/3907613845/MGR_UT1.BAK/vcast_preprocess.8220.18.c
+++
@@ -1,9 +0,0 @@
-
-typedef int VECTORCAST_MARKER__UNIT_PREFIX_START;
-
-typedef int VECTORCAST_MARKER__UNIT_PREFIX_END;
-#include "C:/VCAST/Training/SourceFiles/manager.c"
-
-typedef int VECTORCAST_MARKER__UNIT_APPENDIX_START;
-
-typedef int VECTORCAST_MARKER__UNIT_APPENDIX_END;
